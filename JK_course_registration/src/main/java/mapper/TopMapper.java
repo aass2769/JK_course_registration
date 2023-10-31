1b487b159ba67558c7c22678333b9932e7998215
hyemi951111 <yuwie3408@gmail.com>
2023-10-31 17:51:18 +0900

    topService 수정 완료

--- JK_course_registration/src/main/java/mapper/TopMapper.java
+++ JK_course_registration/src/main/java/mapper/TopMapper.java
@@ -8,7 +8,7 @@ import beans.CourseBean;
 
 public interface TopMapper {
 
-	@Select("select cr_key,cr_course from course_table")
+	@Select("select cr_course from course_table")
 	List<CourseBean> courseList();
 	
 }
--- JK_course_registration/src/main/java/service/TopService.java
+++ JK_course_registration/src/main/java/service/TopService.java
@@ -1,5 +1,6 @@
 package service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +12,39 @@ import dao.TopDao;
 @Service
 public class TopService {
 
-	@Autowired
-	private TopDao topDao;
-	
-	public List<CourseBean> courseList(){
-		
-		List<CourseBean> course_list = topDao.courseList();
-	
-		return course_list;
-	}
+   @Autowired
+   private TopDao topDao;
+   
+   public List<CourseBean> courseList(){
+      
+      List<CourseBean> course_list = topDao.courseList();
+      List<CourseBean> check_list = new ArrayList<CourseBean>();
+      
+      for (int i = 0; i < course_list.size(); i++) {
+           int check_cr_key = course_list.get(i).getCr_key();
+           String check_cr_course = course_list.get(i).getCr_course();
+
+           // 중복 검사용 플래그 변수
+           boolean isDuplicate = false;
+
+           for (int j = 0; j < check_list.size(); j++) {
+               CourseBean check_bean = check_list.get(j);
+
+               if (check_bean.getCr_key() != check_cr_key && check_bean.getCr_course().equals(check_cr_course)) {
+                   isDuplicate = true;
+                   break; // 중복 발견
+               }
+           }
+
+           if (!isDuplicate) {
+               // 중복이 없을 때만 check_list에 추가
+               CourseBean newCheckBean = new CourseBean();
+               newCheckBean.setCr_key(check_cr_key);
+               newCheckBean.setCr_course(check_cr_course);
+               check_list.add(newCheckBean);
+           }
+       }
+      
+      return check_list;
+   }
 }
